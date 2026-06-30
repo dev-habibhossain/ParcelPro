61f609f254e20f6e61384b8260074fde0e4c52a0
Habib Hossain <habibfahim71@gmail.com>
2026-07-01 04:41:12 +0600

    review component are full done

--- client/src/pages/Home/Reviews/ReviewCard.jsx
+++ client/src/pages/Home/Reviews/ReviewCard.jsx
@@ -1,20 +1,25 @@
 import reviewCuote from "../../../assets/reviewQuote.png";
-export default function ReviewCard({ review }) {
+
+export default function ReviewCard({ review, isActive }) {
   const { userName, review: testimonial, user_photoURL, user_email } = review;
+
   return (
-    <div className="max-w-sm bg-base-100 shadow-lg rounded-xl p-6 border border-gray-200">
+    <div className="max-w-sm bg-base-100 rounded-xl p-6 border border-gray-200 mx-auto">
       <img
         src={reviewCuote}
-        className="text-secondary text-2xl mb-4 opacity-70"
+        className="text-secondary text-2xl mb-4"
         alt="quote"
       />
       <p className="text-neutral leading-relaxed mb-4">{testimonial}</p>
       <div className="border-t border-dashed border-secondary pb-4"></div>
 
       <div className="flex items-center gap-4">
-        
-          <img className="w-10 h-10 rounded-full bg-primary" src={user_photoURL} alt="" />
-        
+        <img
+          className="w-10 h-10 rounded-full bg-primary"
+          src={user_photoURL}
+          alt=""
+        />
+
         <div>
           <h3 className="font-semibold text-lg">{userName}</h3>
           <p className="text-sm text-neutral">{user_email}</p>
--- client/src/pages/Home/Reviews/Reviews.jsx
+++ client/src/pages/Home/Reviews/Reviews.jsx
@@ -1,27 +1,46 @@
 import { use } from "react";
-import { EffectCoverflow, Pagination } from "swiper/modules";
+import { Autoplay, EffectCoverflow, Pagination } from "swiper/modules";
+import cutomerTop from "../../../assets/customer-top.png";
 import { Swiper, SwiperSlide } from "swiper/react";
 import ReviewCard from "./ReviewCard";
 export default function Reviews({ reviewsPromise }) {
   const reviews = use(reviewsPromise);
   return (
-    <section>
-      <div></div>
+    <div className="mb-24">
+      <div className="text-center mb-24">
+        <div className="flex justify-center">
+          <img src={cutomerTop} alt="customer-top" />
+        </div>
+        <h3 className="text-3xl text-center text-secondary font-bold py-4">
+          What our customers are sayings
+        </h3>
+        <p className="text-neutral text-sm ">
+          Enhance posture, mobility, and well-being effortlessly with Posture
+          Pro. Achieve proper alignment, reduce <br /> pain, and strengthen your
+          body with ease!
+        </p>
+      </div>
 
       <Swiper
+        loop={true}
         effect={"coverflow"}
         grabCursor={true}
         centeredSlides={true}
-        slidesPerView={3}
+        slidesPerView={2}
         coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
+          rotate: 30,
+          stretch: '50%',
+          depth: 200,
           modifier: 1,
+          scale: 1,
           slideShadows: true,
         }}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
         pagination={true}
-        modules={[EffectCoverflow, Pagination]}
+        modules={[EffectCoverflow, Pagination, Autoplay]}
       >
         {reviews.map((review) => (
           <SwiperSlide key={review.id}>
@@ -29,6 +48,6 @@ export default function Reviews({ reviewsPromise }) {
           </SwiperSlide>
         ))}
       </Swiper>
-    </section>
+    </div>
   );
 }
